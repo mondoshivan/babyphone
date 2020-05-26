dd59bbc22d42447dd3c9c473b7ed9c935aaaa58c
Oliver-Alexander Schmidt <o.schmidt@steinberg.de>
2020-05-26 17:43:32 +0200

    settings component

---
+++ angular/src/app/settings/settings.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'bp-settings',
+  templateUrl: './settings.component.html',
+  styleUrls: ['./settings.component.sass']
+})
+export class SettingsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

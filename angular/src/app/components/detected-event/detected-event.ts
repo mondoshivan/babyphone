95fb2211384980ec7edfce74f66ed7737a7a3c8a
Oliver-Alexander Schmidt <o.schmidt@steinberg.de>
2020-07-08 13:14:31 +0200

    date calculation on server

--- angular/src/app/components/detected-event/detected-event.ts
+++ angular/src/app/components/detected-event/detected-event.ts
@@ -5,11 +5,7 @@ export class DetectedEvent {
   date: string;
 
   constructor(public timestamp:number, public volume:number) {
-    const dateObject = new Date(timestamp * 1000);
-    const hours = dateObject.getHours();
-    const minutes = "0" + dateObject.getMinutes();
-    const seconds = "0" + dateObject.getSeconds();
-    this.date = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+
   }
 
 }
